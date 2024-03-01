637592c0dab13da3d3e1282d0d2c21b25ebac0db
Zelly Snyder <zelcon@zelcon.net>
2024-02-29 21:12:03 -0800

    fix directories

--- hosts/gpu-server-01/zelcon.net-vpn.nix
+++ hosts/gpu-server-01/zelcon.net-vpn.nix
@@ -1,8 +1,8 @@
 { config, ... }:
 let
-  net_zelcon_wg_endpoint = ../../eval-time-secrets/net_zelcon_wg_endpoint;
+  net_zelcon_wg_endpoint = ../../secrets/eval-time-secrets/net_zelcon_wg_endpoint;
   endpoint = builtins.readFile net_zelcon_wg_endpoint;
-  net_zelcon_wg_pubkey = ../../eval-time-secrets/net_zelcon_wg_pubkey;
+  net_zelcon_wg_pubkey = ../../secrets/eval-time-secrets/net_zelcon_wg_pubkey;
   pubkey = builtins.readFile net_zelcon_wg_pubkey;
 in
 {
