70f93944997ec9bc3e85e550afd86b2ea262b224
Zelly Snyder <zelcon@zelcon.net>
2024-06-24 19:59:37 -0700

    [emacs] Fixes for macOS Finder

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,7 +1,30 @@
-;; font
-(defconst +zelcon/font-name+ "FiraCode Nerd Font")
-(set-face-attribute 'default nil :family +zelcon/font-name+ :height 120)
-
 ;; initial Emacs frame size
 (add-to-list 'initial-frame-alist '(width . 120))
 (add-to-list 'initial-frame-alist '(height . 100))
+
+(defun zelcon/make-path (&rest strings)
+  (mapconcat #'identity strings ":"))
+
+(when (eq system-type 'darwin)
+  ;; Launched as a GUI from Finder, Emacs does not inherit shell env, so must be set here
+  (setenv "LIBRARY_PATH"
+          (zelcon/make-path
+           (getenv "LIBRARY_PATH")
+           (concat (getenv "HOME") "/.local/lib")
+           "/usr/lib" "/usr/local/lib" "/opt/homebrew/lib" "/opt/homebrew/lib/gcc/current"
+           "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/lib"))
+  (setenv "PATH"
+          (zelcon/make-path
+           (getenv "PATH")
+           (concat (getenv "HOME") "/.local/bin")
+           "/usr/local/bin" "/opt/homebrew/bin"))
+  (setenv "MANPATH"
+          (zelcon/make-path
+           (getenv "MANPATH")
+           (concat (getenv "HOME") "/.local/lib/share/man")
+           "/opt/homebrew/share/man" "/usr/share/man" "/usr/local/share/man"))
+  (setenv "INFOPATH"
+          (zelcon/make-path
+           (getenv "INFOPATH")
+           (concat (getenv "HOME") "/.local/lib/share/info")
+           "/opt/homebrew/share/info")))
