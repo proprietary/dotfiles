846093d683895ae28355f00a53566e9175d6d39f
Zelly Snyder <zelcon@zelcon.net>
2025-02-24 21:04:25 -0800

    [emacs] Generate random hex array

--- emacs/site-lisp/zelcon--util.el
+++ emacs/site-lisp/zelcon--util.el
@@ -4,4 +4,13 @@
       (/ 100.0 (+ odds 100.0))
     (/ (* -1.0 odds) (+ (* -1.0 odds) 100.0))))
 
+(defun zelcon/generate-random-hex-array (length)
+  "Generate an array of LENGTH random hexadecimal bytes."
+  (interactive "nArray length: ")
+  (let* ((random-bytes (cl-loop for i from 1 to length
+                              collect (random 256)))
+         (hex-strings (mapcar (lambda (byte) (format "0x%02X" byte)) random-bytes))
+         (array-content (mapconcat 'identity hex-strings ", ")))
+    array-content))
+
 (provide 'zelcon--util)
